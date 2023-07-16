99b60893a31ede0dd09ee42f2371647119a59ec4
hasee-k <haseekumanayake@gmail.com>
2023-07-16 23:29:17 +0530

    Initial commit

---
+++ GroupProject/GroupProject/GroupProject/View/Login.xaml.cs
@@ -0,0 +1,89 @@
+﻿using GroupProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GroupProject.View
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_AdminLogin(object sender, RoutedEventArgs e)
+        {
+            var ID = text_username.Text;
+            var Password = text_password.Password;
+
+            using (DataBaseContext Dcontext = new DataBaseContext())
+            {
+                bool CorrectAdminfound = Dcontext.Users.Any(user => user.ID == ID && user.Password == Password);
+                if (CorrectAdminfound)
+                    {
+                        CorrectAdmin();
+                        Close();
+                    }
+
+                else
+                {
+                    MessageBox.Show("Incorrect username or passeword!", "Warning!");
+                }
+            }
+        }
+
+        private void Button_Click_NormalUserLogin(object sender, RoutedEventArgs e)
+        {
+            var ID = text_username.Text;
+            var Password = text_password.Password;
+
+            using (DataBaseContext Dcontext = new DataBaseContext())
+            {
+                bool CorrectAdminfound = Dcontext.Teachers.Any(teacher => teacher.UserName == ID && teacher.Password == Password);
+                if (CorrectAdminfound)
+                {
+                    CorrectNormalUser();
+                    Close();
+                }
+
+                else
+                {
+                    MessageBox.Show("Incorrect username or passeword!", "Warning!");
+                }
+            }
+        }
+
+
+        public void CorrectAdmin()
+        {
+            AdminUserRegistration adminView = new AdminUserRegistration();
+            adminView.Show();
+        }
+        public void CorrectNormalUser()
+        {
+            TeacherDashboard Dashboard = new TeacherDashboard();
+            Dashboard.Show();
+        }
+
+        private void TextBlock_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            MessageBox.Show("Contact IT Admin", "Error");
+        }
+    }
+}
+
+    
---
+++ GroupProject/GroupProject/GroupProject/ViewModel/UserRegistrationVM.cs
@@ -0,0 +1,80 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using GroupProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows;
+
+namespace GroupProject.ViewModel
+{
+    
+        public class UserRegistrationVM : ObservableObject
+        {
+            private string _username;
+            private string _password;
+            private string _teacherRegNum;
+            private string _email;
+            private string _name;
+
+            public string Username
+            {
+                get => _username;
+                set => SetProperty(ref _username, value);
+            }
+
+            public string Password
+            {
+                get => _password;
+                set => SetProperty(ref _password, value);
+            }
+
+            public string RegNumber
+            {
+                get => _teacherRegNum;
+                set => SetProperty(ref _teacherRegNum, value);
+            }
+
+            public string Email
+            {
+                get => _email;
+                set => SetProperty(ref _email, value);
+            }
+
+            public string Name
+            {
+                get => _name;
+                set => SetProperty(ref _name, value);
+            }
+
+
+            public ICommand RegisterUserCommand { get; }
+
+            public UserRegistrationVM()
+            {
+                RegisterUserCommand = new RelayCommand(RegisterUser);
+            }
+
+            private void RegisterUser()
+            {
+                using (var context = new DataBaseContext())
+                {
+                    if (context.Teachers.Any(u => u.UserName == Username))
+                    {
+                        MessageBox.Show("A user with the same username already exists.");
+                    }
+                    else
+                    {
+                        context.Teachers.Add(new NormalUser() { UserName = Username, Password = Password, Email = Email, ID = RegNumber, Name = Name });
+                        context.SaveChanges();
+
+                        MessageBox.Show("User is Added Successfully!");
+                    }
+                }
+            }
+        }
+    }
+
