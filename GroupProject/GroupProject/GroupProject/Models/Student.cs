e46c4dd674a2ec27bdf60e4a5f9f1f2fb9a9b7eb
hasee-k <139696038+ShashiniUK@users.noreply.github.com>
2023-09-26 12:22:46 +0530

    changes

---
+++ GroupProject/GroupProject/GroupProject/Models/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupProject.Models
+{
+    public class Student
+    {
+       
+        [Key]
+        public string StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }   
+        
+        
+        public double GPA { get; set; }
+
+       
+
+    }
+}
